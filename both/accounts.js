207c045b3d87f9499c08dd3936284a7b5f12d410
Jay <jtolla@Hannahs-iMac.local>
2015-09-25 10:15:56 -0400

    Initial commit

--- both/accounts.js
+++ both/accounts.js
@@ -1,15 +1,19 @@
-AccountsTemplates.configureRoute('signIn', {
-    layoutTemplate: 'appLayout'
-});
-
-AccountsTemplates.configureRoute('signUp', {
-    layoutTemplate: 'appLayout'
-});
-
 AccountsTemplates.configure({
     texts: {
         title: {
             signUp: "Sign Up"
         },
-    },
+    }
 });
+
+AccountsTemplates.configureRoute('signIn', {
+	template: 'signIn',
+    layoutTemplate: 'appLayout'
+});
+
+AccountsTemplates.configureRoute('signUp', {
+	template: 'signUp',
+    layoutTemplate: 'appLayout'
+});
+
+
